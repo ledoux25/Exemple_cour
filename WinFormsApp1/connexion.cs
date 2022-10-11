d7d80fad9a88bb8ce06ae3fd78fc1a3c8380ad2c
Ledoux Sanguo <sanguojoseph17@gmail.com>
2022-10-11 11:49:18 +0100

    formulaire de connexion 1.0

---
+++ WinFormsApp1/connexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class connexion : Form
+    {
+        public connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connexion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
